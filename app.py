62feb33e0435f3bf75ab8124b5f059e370691dcd
Justin Carter <justincarter@Hannahs-MacBook-Air.local>
2020-06-09 18:52:31 -0500

    Initial commit.

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot.trainers import ListTrainer
+
+app = Flask(__name__)
+
+chatbot = ChatBot('Ron Obvious')
+
+# Create a new trainer for the chatbot
+trainer = ChatterBotCorpusTrainer(chatbot)
+
+# Train the chatbot based on the english corpus
+trainer.train("chatterbot.corpus.english")
+
+# Get a response to an input statement
+
+@app.route("/")
+def home():    
+    return render_template("home.html") 
+@app.route("/get")
+def get_bot_response():    
+    userText = request.args.get('msg')    
+    return str(chatbot.get_response(userText)) 
+if __name__ == "__main__":    
+    app.run()
